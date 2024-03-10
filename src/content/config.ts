56720554c7cef03be3ee35ec6d3ea4fbef71ee97
Luke Storry <luke.storry@letsdothis.com>
2024-03-10 17:03:33 +0000

    basic coursecard homepage

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,23 @@
 import { z, defineCollection } from "astro:content";
 
+const courseInfoSchema = z.object({
+  title: z.string(),
+  order: z.number().optional(),
+  description: z.string(),
+  slug: z.string(),
+  image: z
+    .object({
+      src: z.string(),
+      alt: z.string(),
+    })
+    .optional(),
+});
 const coursesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    order: z.number().optional(),
-    description: z.string(),
-    image: z
-      .object({
-        url: z.string(),
-        alt: z.string(),
-      })
-      .optional(),
-  }),
+  schema: courseInfoSchema,
 });
+
+export type CourseInfo = z.infer<typeof courseInfoSchema>;
 
 export const collections = {
   courses: coursesCollection,
