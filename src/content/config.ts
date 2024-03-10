ac0bd3ab32446986656e3ac313b15c1697384ebb
Luke Storry <luke.storry@letsdothis.com>
2024-03-10 17:22:19 +0000

    tidy image schema for collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,20 @@
 import { z, defineCollection } from "astro:content";
 
-const courseInfoSchema = z.object({
-  title: z.string(),
-  order: z.number().optional(),
-  description: z.string(),
-  slug: z.string(),
-  image: z
-    .object({
-      src: z.string(),
-      alt: z.string(),
-    })
-    .optional(),
-});
 const coursesCollection = defineCollection({
   type: "content",
-  schema: courseInfoSchema,
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      order: z.number().optional(),
+      description: z.string(),
+      image: z
+        .object({
+          src: image(),
+          alt: z.string(),
+        })
+        .optional(),
+    }),
 });
-
-export type CourseInfo = z.infer<typeof courseInfoSchema>;
 
 export const collections = {
   courses: coursesCollection,
