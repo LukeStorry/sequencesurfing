7bdd42dacfae2fbb550b151718a58e77f63c1084
Luke Storry <luke.storry@letsdothis.com>
2024-03-10 17:49:36 +0000

    add mooli body font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Original Surfer", ...defaultTheme.fontFamily.sans],
+        body: ["Mooli", ...defaultTheme.fontFamily.sans],
       },
     },
   },
