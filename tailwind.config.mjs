eb13dfc23938ac0f39c474b58e3ceea814075417
Luke Storry <luke.storry@letsdothis.com>
2024-03-30 17:27:21 +0000

    cleanup tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,10 +14,6 @@ export default {
       },
     },
   },
-  safelist: [
-    "!duration-[0ms]",
-    "!delay-[0ms]",
-    'html.js :where([class*="taos:"]:not(.taos-init))',
-  ],
+  safelist: ['html.js :where([class*="taos:"]:not(.taos-init))'],
   plugins: [require("@tailwindcss/typography"), require("taos/plugin")],
 };
